c3191136406dbeea437781497f681b498ea10b64
Waylon Walker <quadmx08@gmail.com>
2021-04-22 16:36:36 -0500

    update plugins.lua

---
+++ dotfiles/.config/nvim/plugins.lua
@@ -0,0 +1,6 @@
+require'lspconfig'.pyright.setup{}
+require'lspconfig'.bashls.setup{}
+require'lualine'.setup{
+    options = {  section_separators = {'>', '>>'},  component_separators = {'|', '||'}}
+}
+
