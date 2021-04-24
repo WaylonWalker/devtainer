b0978ce4f5fcb41076475e976ae55909821787a3
Waylon Walker <quadmx08@gmail.com>
2021-04-23 21:04:21 -0500

    configure lualine and tree-sitter

--- dotfiles/.config/nvim/plugins.lua
+++ dotfiles/.config/nvim/plugins.lua
@@ -1,6 +1,40 @@
 require'lspconfig'.pyright.setup{}
 require'lspconfig'.bashls.setup{}
 require'lualine'.setup{
-    options = {  section_separators = {'>', '>>'},  component_separators = {'|', '||'}}
+    options = {
+        theme='onedark',
+    section_separators = {'', ''},
+    component_separators = {'|', '|'},
+    sections = {
+    -- lualine_a = {'mode'},
+    -- lualine_b = {'branch'},
+    -- lualine_c = {'filename'},
+    -- lualine_x = {'encoding', 'fileformat', 'filetype'},
+    -- lualine_y = {'progress'},
+    -- lualine_z = {'location'}
+    },
+    inactive_sections = {
+    -- lualine_a = {},
+    -- lualine_b = {},
+    -- lualine_c = {'filename'},
+    -- lualine_x = {'location'},
+    -- lualine_y = {},
+    -- lualine_z = {}
+    }
+    }
 }
 
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = {
+    "python",
+    "regex",
+    "lua",
+    "javascript",
+    "bash",
+    "toml",
+    "rst",
+    "html",
+    "json",
+    "yaml"
+  } -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+}
