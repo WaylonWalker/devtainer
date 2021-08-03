f6dae8d611b7d788b8fa193439186d8238bbd444
Waylon S. Walker <walker_waylon_s@cat.com>
2021-08-03 08:36:37 -0500

    add tmux/nvim

--- update.sh
+++ update.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+cp -a ~/.config/nvim/. ./dotfiles/.config/nvim
 cp -a ~/.config/. ./dotfiles/.config
 cp ~/.tmux.conf ./dotfiles/
 cp ~/.pdbrc ./dotfiles/
 cp ~/.zshrc ./dotfiles/
+cp ~/.tmux.conf ./dotfiles/.tmux.conf
 cp -a ~/.vifm/. ./dotfiles/.vifm
 cp ~/.alias ./dotfiles/
 
