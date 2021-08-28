e92e95113932c1b1f4dc5e5a6a7b035f678e139a
Waylon S. Walker <walker_waylon_s@cat.com>
2021-08-28 10:58:53 -0500

    write title to file

---
+++ nvim/.config/nvim/lua/waylonwalker/title.lua
@@ -0,0 +1,17 @@
+local M = {}
+local api = vim.api
+local Path = require("plenary.path")
+
+M.write_title = function()
+    title = vim.api.nvim_get_current_line():gsub('#', '')
+    Path:new('~/.config/title/title.txt'):write(title, 'w')
+    local pipe = io.popen('tmux source-file ~/.tmux.conf')
+    vim.defer_fn(function()
+        pipe:flush()
+        pipe:close()
+    end, 250)
+end
+
+api.nvim_set_keymap('n', '<leader><leader>t', ":lua require'waylonwalker.title'.write_title()<cr>", { noremap = true, silent = true })
+
+return M
