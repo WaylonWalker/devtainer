6a4d9145ff66e53767af1ddbd9e0914760fba83d
Waylon S. Walker <walker_waylon_s@cat.com>
2021-08-28 15:19:49 -0500

    wip awesome rc

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -664,7 +664,6 @@ end)
 
 
 awful.spawn.with_shell('compton')
--- awful.spawn.with_shell('nitrogen --restore')
 awful.spawn.with_shell('feh --bg-scale ~/Pictures/wp2555882-space-background.jpg')
 awful.spawn.with_shell('launch_polybar')
 awful.spawn.with_shell('flameshot')
