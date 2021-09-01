7a31540a33e897c35f602698ce33b26662881df8
Waylon S. Walker <walker_waylon_s@cat.com>
2021-08-31 21:53:09 -0500

    wip awesome keys

--- awesome/.config/awesome/keymap.lua
+++ awesome/.config/awesome/keymap.lua
@@ -13,10 +13,25 @@ modkey = "Mod4"
 
 -- mostly default
 globalkeys = gears.table.join(
-	awful.key({ modkey }, "s", hotkeys_popup.show_help, { description = "show help", group = "awesome" }),
-	awful.key({ modkey }, "Left", awful.tag.viewprev, { description = "view previous", group = "tag" }),
-	awful.key({ modkey }, "Right", awful.tag.viewnext, { description = "view next", group = "tag" }),
-	awful.key({ modkey }, "Escape", awful.tag.history.restore, { description = "go back", group = "tag" }),
+	awful.key({ modkey }, "s", hotkeys_popup.show_help, {
+		description = "show help",
+		group = "awesome",
+	}),
+
+	awful.key({ modkey }, "Left", awful.tag.viewprev, {
+		description = "view previous",
+		group = "tag",
+	}),
+    
+	awful.key({ modkey }, "Right", awful.tag.viewnext, {
+		description = "view next",
+		group = "tag",
+	}),
+
+	awful.key({ modkey }, "Escape", awful.tag.history.restore, {
+		description = "go back",
+		group = "tag",
+	}),
 
 	awful.key({ modkey }, "j", function()
 		awful.client.focus.byidx(1)
@@ -24,6 +39,7 @@ globalkeys = gears.table.join(
 		description = "focus next by index",
 		group = "client",
 	}),
+
 	awful.key({ modkey }, "k", function()
 		awful.client.focus.byidx(-1)
 	end, {
@@ -38,25 +54,33 @@ globalkeys = gears.table.join(
 		description = "swap with next client by index",
 		group = "client",
 	}),
+    
 	awful.key({ modkey, "Shift" }, "k", function()
 		awful.client.swap.byidx(-1)
 	end, {
 		description = "swap with previous client by index",
 		group = "client",
 	}),
+
 	awful.key({ modkey, "Control" }, "j", function()
 		awful.screen.focus_relative(1)
 	end, {
 		description = "focus the next screen",
 		group = "screen",
 	}),
+    
 	awful.key({ modkey, "Control" }, "k", function()
 		awful.screen.focus_relative(-1)
 	end, {
 		description = "focus the previous screen",
 		group = "screen",
 	}),
-	awful.key({ modkey }, "u", awful.client.urgent.jumpto, { description = "jump to urgent client", group = "client" }),
+    
+	awful.key({ modkey }, "u", awful.client.urgent.jumpto, {
+		description = "jump to urgent client",
+		group = "client",
+	}),
+
 	awful.key({ modkey }, "Tab", function()
 		awful.client.focus.history.previous()
 		if client.focus then
@@ -67,9 +91,15 @@ globalkeys = gears.table.join(
 		group = "client",
 	}),
 
-	awful.key({ modkey, "Control" }, "r", awesome.restart, { description = "reload awesome", group = "awesome" }),
+	awful.key({ modkey, "Control" }, "r", awesome.restart, {
+		description = "reload awesome",
+		group = "awesome",
+	}),
 
-	awful.key({ modkey, "Shift" }, "q", awesome.quit, { description = "quit awesome", group = "awesome" }),
+	awful.key({ modkey, "Shift" }, "q", awesome.quit, {
+		description = "quit awesome",
+		group = "awesome",
+	}),
 
 	awful.key({ modkey }, "l", function()
 		awful.tag.incmwfact(0.05)
@@ -77,36 +107,42 @@ globalkeys = gears.table.join(
 		description = "increase master width factor",
 		group = "layout",
 	}),
+
 	awful.key({ modkey }, "h", function()
 		awful.tag.incmwfact(-0.05)
 	end, {
 		description = "decrease master width factor",
 		group = "layout",
 	}),
+
 	awful.key({ modkey, "Shift" }, "h", function()
 		awful.tag.incnmaster(1, nil, true)
 	end, {
 		description = "increase the number of master clients",
 		group = "layout",
 	}),
+
 	awful.key({ modkey, "Shift" }, "l", function()
 		awful.tag.incnmaster(-1, nil, true)
 	end, {
 		description = "decrease the number of master clients",
 		group = "layout",
 	}),
+
 	awful.key({ modkey, "Control" }, "h", function()
 		awful.tag.incncol(1, nil, true)
 	end, {
 		description = "increase the number of columns",
 		group = "layout",
 	}),
+
 	awful.key({ modkey, "Control" }, "l", function()
 		awful.tag.incncol(-1, nil, true)
 	end, {
 		description = "decrease the number of columns",
 		group = "layout",
 	}),
+
 	awful.key({ modkey }, "space", function()
 		awful.layout.inc(1)
 		os.execute("echo " .. awful.layout.getname() .. " > ~/.config/awesome/layout.txt")
@@ -114,6 +150,7 @@ globalkeys = gears.table.join(
 		description = "select next",
 		group = "layout",
 	}),
+
 	awful.key({ modkey, "Shift" }, "space", function()
 		awful.layout.inc(-1)
 		os.execute("echo " .. awful.layout.getname() .. " > ~/.config/awesome/layout.txt")
@@ -156,30 +193,40 @@ clientkeys = gears.table.join(
 		description = "toggle fullscreen",
 		group = "client",
 	}),
+
 	awful.key({ modkey }, "c", function(c)
 		c:kill()
-	end, { description = "close", group = "client" }),
-	awful.key(
-		{ modkey, "Control" },
-		"space",
-		awful.client.floating.toggle,
-		{ description = "toggle floating", group = "client" }
-	),
+	end, {
+		description = "close",
+		group = "client",
+	}),
+
+	awful.key({ modkey, "Control" }, "space", awful.client.floating.toggle, {
+		description = "toggle floating",
+		group = "client",
+	}),
+
 	awful.key({ modkey, "Control" }, "Return", function(c)
 		c:swap(awful.client.getmaster())
 	end, {
 		description = "move to master",
 		group = "client",
 	}),
+
 	awful.key({ modkey }, "o", function(c)
 		c:move_to_screen()
-	end, { description = "move to screen", group = "client" }),
+	end, {
+		description = "move to screen",
+		group = "client",
+	}),
+
 	awful.key({ modkey }, "t", function(c)
 		c.ontop = not c.ontop
 	end, {
 		description = "toggle keep on top",
 		group = "client",
 	}),
+
 	awful.key({ modkey }, "n", function(c)
 		-- The client currently has the input focus, so it cannot be
 		-- minimized, since minimized clients can't have the focus.
@@ -188,6 +235,7 @@ clientkeys = gears.table.join(
 		description = "minimize",
 		group = "client",
 	}),
+
 	awful.key({ modkey }, "m", function(c)
 		c.maximized = not c.maximized
 		c:raise()
@@ -195,6 +243,7 @@ clientkeys = gears.table.join(
 		description = "(un)maximize",
 		group = "client",
 	}),
+
 	awful.key({ modkey, "Control" }, "m", function(c)
 		c.maximized_vertical = not c.maximized_vertical
 		c:raise()
@@ -202,6 +251,7 @@ clientkeys = gears.table.join(
 		description = "(un)maximize vertically",
 		group = "client",
 	}),
+
 	awful.key({ modkey, "Shift" }, "m", function(c)
 		c.maximized_horizontal = not c.maximized_horizontal
 		c:raise()
@@ -209,12 +259,15 @@ clientkeys = gears.table.join(
 		description = "(un)maximize horizontally",
 		group = "client",
 	}),
+
 	awful.key({ modkey }, "p", function()
 		xrandr.xrandr()
 	end),
+
 	awful.key({ modkey, "Mod1" }, "p", function()
 		awful.spawn("flameshot gui")
 	end),
+
 	awful.key({}, "Print", function()
 		awful.spawn("flameshot gui")
 	end)
@@ -328,11 +381,17 @@ globalkeys = gears.table.join(
 
 	awful.key({ modkey }, "t", function()
 		awful.spawn("teams")
-	end, { description = "teams chat", group = "launcher" }),
+	end, {
+		description = "teams chat",
+		group = "launcher",
+	}),
 
 	awful.key({ modkey }, "e", function()
 		awful.spawn("nautilus")
-	end, { description = "teams chat", group = "launcher" }),
+	end, {
+		description = "teams chat",
+		group = "launcher",
+	}),
 
 	awful.key({ modkey, "Shift" }, "o", function()
 		awful.spawn("obs-studio")
@@ -368,22 +427,41 @@ globalkeys = gears.table.join(
 	-- Media Keys
 	awful.key({}, "XF86AudioRaiseVolume", function()
 		os.execute("pactl set-sink-volume 0 +5%")
-	end),
+	end, {
+    description = "raise volume",
+    group= "media",
+}),
 	awful.key({}, "XF86AudioLowerVolume", function()
 		os.execute("pactl set-sink-volume 0 -5%")
-	end),
+	end, {
+    description = "lower volume",
+    group= "media",
+}),
 	awful.key({}, "XF86AudioMute", function()
 		os.execute("pactl set-sink-mute 0 toggle")
-	end),
+	end, {
+    description = "mute-volume",
+    group = "media",
+}),
 	awful.key({}, "XF86AudioPlay", function()
 		os.execute("playerctl play-pause")
-	end),
+	end, {
+    description = 'play-pause',
+    group = 'media'
+
+}),
 	awful.key({}, "XF86AudioNext", function()
 		os.execute("playerctl next")
-	end),
+	end, {
+    description = 'media next',
+    group  = 'media',
+}),
 	awful.key({}, "XF86AudioPrev", function()
 		os.execute("playerctl previous")
-	end)
+	end, {
+    description = 'media previous',
+    group = 'media',
+})
 )
 
 clientbuttons = gears.table.join(
