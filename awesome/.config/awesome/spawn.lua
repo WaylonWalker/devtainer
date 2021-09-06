1275676c6890a96f8deaf1bfa87283fe81b21c5a
Waylon S. Walker <waylon@waylonwalker.com>
2021-09-06 13:25:24 -0500

    fix background on startup

--- awesome/.config/awesome/spawn.lua
+++ awesome/.config/awesome/spawn.lua
@@ -1,6 +1,6 @@
 local awful = require("awful")
 
 awful.spawn.with_shell("compton")
-awful.spawn.with_shell("feh --bg-scale ~/.config/awesome/wp2555882-space-background.jpg")
+awful.spawn.with_shell("feh --bg-scale ~/.config/awesome/wallpaper/wp2555882-space-background.jpg")
 awful.spawn.with_shell("launch_polybar")
 awful.spawn.with_shell("flameshot")
