b360e2e095081a61153ed1ca5dfdfb641ca7d8ca
Waylon S. Walker <waylon@waylonwalker.com>
2021-12-11 09:42:19 -0600

    install rich if missing

--- ipython/.ipython/profile_default/startup/rich_init.py
+++ ipython/.ipython/profile_default/startup/rich_init.py
@@ -1,12 +1,18 @@
 def rich_init():
     from rich import pretty, traceback
+
     pretty.install()
     traceback.install()
+
 
 if __name__ == "__main__":
     try:
         rich_init()
     except ImportError:
-        pass
-
-
+        print("installing rich")
+        subprocess.Popen(
+            ["pip", "install", "rich"],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        ).wait()
+        rich_init()
