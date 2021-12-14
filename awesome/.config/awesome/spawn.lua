e967ae13c4bec7469f52cf9ef86257954e07a2bc
Waylon S. Walker <waylon@waylonwalker.com>
2021-12-13 20:34:28 -0600

    change default background

--- awesome/.config/awesome/spawn.lua
+++ awesome/.config/awesome/spawn.lua
@@ -1,7 +1,7 @@
 local awful = require("awful")
 
 awful.spawn.with_shell("compton")
-awful.spawn.with_shell("feh --bg-scale ~/.config/awesome/wallpaper/wp2555882-space-background.jpg")
+awful.spawn.with_shell("feh --bg-scale ~/.config/awesome/wallpaper/backdrop-distort-color.png")
 awful.spawn.with_shell("launch_polybar")
 awful.spawn.with_shell("flameshot")
 awful.spawn.with_shell("nm-applet")
