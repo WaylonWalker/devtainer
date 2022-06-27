163d392ec6221c347b06a031d02b499323c4e521
Waylon S. Walker <waylon@waylonwalker.com>
2022-06-27 08:49:03 -0500

    update wallpapaer

--- awesome/.config/awesome/spawn.lua
+++ awesome/.config/awesome/spawn.lua
@@ -1,7 +1,7 @@
 local awful = require("awful")
 
 awful.spawn.with_shell("compton")
-awful.spawn.with_shell("feh --bg-scale ~/.config/awesome/wallpaper/backdrop-distort-color.png")
+awful.spawn.with_shell("feh --bg-scale ~/.config/awesome/wallpaper/1440w5.png")
 awful.spawn.with_shell("launch_polybar")
 awful.spawn.with_shell("flameshot")
 awful.spawn.with_shell("nm-applet")
