e96a96e1ad9a71a07ff7fa6320c728480300a49a
Waylon S. Walker <waylon@waylonwalker.com>
2022-06-29 09:44:42 -0500

    create wezterm config

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,7 @@
+local wezterm = require 'wezterm';
+return {
+  color_scheme = "Batman",
+  font = wezterm.font("JetBrains Mono"),
+  hide_tab_bar_if_only_one_tab = true,
+  window_background_opacity = .5,
+}
