28367f43c368c946b5305516c1db46b0db750951
Waylon S. Walker <waylon@waylonwalker.com>
2022-10-07 19:19:39 -0500

    nice rouded cmp borders

--- nvim/.config/nvim/lua/waylonwalker/cmp.lua
+++ nvim/.config/nvim/lua/waylonwalker/cmp.lua
@@ -14,6 +14,9 @@ snippet = {
     -- vim.fn["UltiSnips#Anon"](args.body)
     end,
 },
+window = {
+    completion = cmp.config.window.bordered(),
+},
 mapping = {
   ['<C-n>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
   ['<C-p>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
