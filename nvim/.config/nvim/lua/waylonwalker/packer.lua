dcf74b5696a8d4b0cb0327ce5e09cade82ecebd7
Waylon S. Walker <waylon@waylonwalker.com>
2023-01-18 09:22:44 -0600

    add trouble.nvim

--- nvim/.config/nvim/lua/waylonwalker/packer.lua
+++ nvim/.config/nvim/lua/waylonwalker/packer.lua
@@ -23,6 +23,17 @@ vim.cmd([[packadd packer.nvim]])
 return require("packer").startup(function(use)
     use("whiteinge/diffconflicts")
     use({
+        "folke/trouble.nvim",
+        requires = "kyazdani42/nvim-web-devicons",
+        config = function()
+            require("trouble").setup({
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            })
+        end,
+    })
+    use({
         "andythigpen/nvim-coverage",
         requires = "nvim-lua/plenary.nvim",
         config = function()
