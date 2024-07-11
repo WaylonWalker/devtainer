bb0ce9782b9be9990379f8f2a390974f981098da
Waylon S. Walker <waylon@waylonwalker.com>
2024-07-11 08:43:33 -0500

    fix zlong alert

--- zsh/.zsh/plugins/zlong_alert/zlong_alert.zsh
+++ zsh/.zsh/plugins/zlong_alert/zlong_alert.zsh
@@ -6,7 +6,7 @@ zlong_duration=.1
 
 # Need to set an initial timestamps otherwise, we'll be comparing an empty
 # string with an integer.
-zlong_timestamp=$EPOCHSECONDS
+zlong_timestamp=`date +%s`
 
 # Define the alerting function, do the text processing here
 zlong_alert_func() {
@@ -24,12 +24,13 @@ zlong_alert_func() {
 }
 
 zlong_alert_pre() {
-    zlong_timestamp=$EPOCHSECONDS
+    zlong_timestamp=`date +%s`
     zlong_last_cmd=$1
 }
 
 zlong_alert_post() {
-    local duration=$(($EPOCHSECONDS - $zlong_timestamp))
+    local now=`date +%s`
+    local duration=$(($now - $zlong_timestamp))
     local lasted_long=$(($duration - $zlong_duration))
     local cmd_head=$(echo $zlong_last_cmd | cut -d ' ' -f 1)
     if [[ $lasted_long -gt 0 && ! -z $zlong_last_cmd && ! $zlong_ignore_cmds =~ $cmd_head ]]; then
@@ -38,5 +39,7 @@ zlong_alert_post() {
     zlong_last_cmd=''
 }
 
+# if add-zsh-hook is not available, use the preexec and precmd functions
+autoload -Uz add-zsh-hook
 add-zsh-hook preexec zlong_alert_pre
 add-zsh-hook precmd zlong_alert_post
