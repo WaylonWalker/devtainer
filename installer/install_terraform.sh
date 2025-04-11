279034031cb2e912c4b421dafe19d4c9f4ae16c6
Waylon S. Walker <waylon@waylonwalker>
2025-04-11 08:51:13 -0500

    release v0.1.2

--- installer/install_terraform.sh
+++ installer/install_terraform.sh
@@ -4,7 +4,7 @@
 curl https://releases.hashicorp.com/terraform/1.9.8/terraform_1.9.8_linux_amd64.zip -o terraform.zip
 unzip terraform.zip
 chmod +x terraform
-if [ /home/waylon/git/devtainer != /usr/local/bin ]; then
+if [ /home/waylon/devtainer != /usr/local/bin ]; then
     mv terraform /usr/local/bin
 fi
 
