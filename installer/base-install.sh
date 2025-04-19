92c9f8988d898c1356931125dd66d0734b3e62e3
Waylon S. Walker <waylon@waylonwalker>
2025-04-18 20:35:37 -0500

    remove sudo

--- installer/base-install.sh
+++ installer/base-install.sh
@@ -32,13 +32,13 @@ PACKAGES=(
 )
 
 install_arch() {
-	sudo pacman -Syu --noconfirm
-	sudo pacman -S --noconfirm base-devel "${PACKAGES[@]/pip/python-pip}"
+	pacman -Syu --noconfirm
+	pacman -S --noconfirm base-devel "${PACKAGES[@]/pip/python-pip}"
 }
 
 install_alpine() {
-	sudo apk update
-	sudo apk add --no-cache alpine-sdk \
+	apk update
+	apk add --no-cache alpine-sdk \
 		bash \
 		"${PACKAGES[@]/pip/py3-pip}" \
 		python3 \
@@ -50,8 +50,8 @@ install_alpine() {
 }
 
 install_debian() {
-	sudo apt-get update
-	sudo apt-get install -y --no-install-recommends \
+	apt-get update
+	apt-get install -y --no-install-recommends \
 		build-essential \
 		"${PACKAGES[@]/pip/python3-pip}" \
 		libbz2-dev \
@@ -59,13 +59,13 @@ install_debian() {
 		libreadline-dev \
 		libsqlite3-dev \
 		libssl-dev
-	sudo apt-get clean
-	sudo rm -rf /var/lib/apt/lists/*
+	apt-get clean
+	rm -rf /var/lib/apt/lists/*
 }
 
 install_fedora() {
-	sudo dnf groupinstall -y "Development Tools"
-	sudo dnf install -y \
+	dnf groupinstall -y "Development Tools"
+	dnf install -y \
 		"${PACKAGES[@]/pip/python3-pip}" \
 		bzip2-devel \
 		glibc-devel \
