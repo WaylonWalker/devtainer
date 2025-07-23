94783614863947a3748168fe5ba7e5a064dde7cf
Waylon S. Walker <waylon@waylonwalker.com>
2025-07-23 10:46:39 -0500

    big cleanup

--- nvim/.config/nvim/lua/waylonwalker/plugins/lsp.lua
+++ nvim/.config/nvim/lua/waylonwalker/plugins/lsp.lua
@@ -1,7 +1,7 @@
 -- lua/plugins/lsp/init.lua (or similar)
 local servers = {
-	"ruff",
 	-- "ty",
+	-- "ruff",
 	"ts_ls",
 	"lua_ls",
 	"jsonls",
@@ -19,12 +19,14 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 for _, server in ipairs(servers) do
-	lspconfig[server].setup({
-		on_attach = function(client, bufnr)
-			-- map keys, format on save, etc.
-		end,
-		capabilities = capabilities,
-	})
+	if not lspconfig[server].manager then
+		lspconfig[server].setup({
+			on_attach = function(client, bufnr)
+				-- map keys, format on save, etc.
+			end,
+			capabilities = capabilities,
+		})
+	end
 end
 
 vim.lsp.config("ty", {
@@ -39,3 +41,24 @@ vim.lsp.config("ty", {
 vim.lsp.enable("ty")
 
 -- vim.fn.sign_define("LspCodeActionSign", { text = "", texthl = "" })
+--
+-- Display diagnostics at end of line using virtual lines (Neovim 0.10+)
+-- if vim.diagnostic.config then
+-- 	vim.diagnostic.config({
+-- 		virtual_text = false,
+-- 		virtual_lines = {
+-- 			only_current_line = false,
+-- 			highlight_whole_line = false,
+-- 		},
+-- 		signs = true,
+-- 		underline = true,
+-- 		update_in_insert = false,
+-- 		severity_sort = true,
+-- 	})
+--
+-- 	-- Toggle inline diagnostics with <leader>ld
+-- 	vim.keymap.set("n", "<leader>ld", function()
+-- 		local current = vim.diagnostic.config().virtual_lines
+-- 		vim.diagnostic.config({ virtual_lines = not current })
+-- 	end, { desc = "Toggle virtual line diagnostics" })
+-- end
