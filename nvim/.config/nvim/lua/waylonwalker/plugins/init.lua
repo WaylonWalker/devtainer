c8bd8a6de0a10d9d9e8b186e5d0528c499006376
Waylon S. Walker <waylon@waylonwalker.com>
2025-07-23 10:46:16 -0500

    use mini.ai

--- nvim/.config/nvim/lua/waylonwalker/plugins/init.lua
+++ nvim/.config/nvim/lua/waylonwalker/plugins/init.lua
@@ -6,4 +6,5 @@ M.lualine = require("waylonwalker.plugins.lualine")
 M.treesitter = require("waylonwalker.plugins.treesitter")
 M.alpha_nvim = require("waylonwalker.plugins.alpha-nvim")
 M.navbuddy = require("waylonwalker.plugins.navbuddy")
+M.mini_ai = require("waylonwalker.plugins.mini-ai")
 return M
