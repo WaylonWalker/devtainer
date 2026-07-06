9c3f70448b436af5969c00ad0899c3043dd9400b
Waylon S. Walker <waylon@waylonwalker.com>
2026-07-06 08:00:42 -0500

    add snacks

--- nvim/.config/nvim/lua/waylonwalker/lazy.lua
+++ nvim/.config/nvim/lua/waylonwalker/lazy.lua
@@ -273,6 +273,8 @@ require("lazy").setup({
 	-- "gc" to comment visual regions/lines
 	{ "numToStr/Comment.nvim", opts = {} },
 
+	{ "folke/snacks.nvim", opts = { picker = { enabled = true } } },
+
 	-- Fuzzy Finder (files, lsp, etc)
 	{
 		"nvim-telescope/telescope.nvim",
